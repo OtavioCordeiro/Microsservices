cb1361790c0aac221167f165d185c32d6b9c2cba
Otavio Cordeiro Lopes <otavio.cordeiro.ext@brf.com>
2022-07-19 21:42:11 -0300

    Adicionar arquivos de projeto.

---
+++ Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,93 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public CatalogController(IProductRepository productRepository)
+        {
+            _repository = productRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _repository.GetProduct(id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            if (category is null)
+                return BadRequest("Invalid category");
+
+            var product = await _repository.GetProductByCategory(category);
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            if (product is null)
+                return BadRequest("Invalid product");
+
+            await _repository.CreateProduct(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        // substituindo o produces response pelo apiconventions de exemplo somente
+        [HttpPut]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Put))]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            if (product is null)
+                return BadRequest("Invalid product");
+
+            return Ok(await _repository.UpdateProduct(product));
+        }
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Product>> DeleteProductById(string id)
+        {
+
+            return Ok(await _repository.DeleteProduct(id));
+        }
+    }
+}
---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,85 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            var p1 = new Product()
+            {
+                Id = "507f191e810c19729de860ea",
+                Name = "Samdung S10e",
+                Description = "Ut sequi officia qui corporis suscipit cum deserunt minus ea quidem expedita et voluptas perferendis sed totam perspiciatis. Et atque officia vel cupiditate rerum ea laboriosam quasi est iste vero est iste cupiditate ea quia doloribus eum unde repellat.",
+                Image = "celulars10e.png",
+                Price = 3500.00M,
+                Category = "Smart Phone"
+
+            };
+
+            var p2 = new Product()
+            {
+                Id = "607f191e810c19729de860ea",
+                Name = "Iphone XR",
+                Description = "Ut sequi officia qui corporis suscipit cum deserunt minus ea quidem expedita et voluptas perferendis sed totam perspiciatis. Et atque officia vel cupiditate rerum ea laboriosam quasi est iste vero est iste cupiditate ea quia doloribus eum unde repellat.",
+                Image = "celulars10e.png",
+                Price = 3500.00M,
+                Category = "Smart Phone"
+
+            };
+
+            var p3 = new Product()
+            {
+                Id = "707f191e810c19729de860ea",
+                Name = "Xiaomi",
+                Description = "Ut sequi officia qui corporis suscipit cum deserunt minus ea quidem expedita et voluptas perferendis sed totam perspiciatis. Et atque officia vel cupiditate rerum ea laboriosam quasi est iste vero est iste cupiditate ea quia doloribus eum unde repellat.",
+                Image = "celulars10e.png",
+                Price = 3500.00M,
+                Category = "Smart Phone"
+
+            };
+
+            var p4 = new Product()
+            {
+                Id = "807f191e810c19729de860ea",
+                Name = "Maquina de Lavar",
+                Description = "Ut sequi officia qui corporis suscipit cum deserunt minus ea quidem expedita et voluptas perferendis sed totam perspiciatis. Et atque officia vel cupiditate rerum ea laboriosam quasi est iste vero est iste cupiditate ea quia doloribus eum unde repellat.",
+                Image = "celulars10e.png",
+                Price = 3500.00M,
+                Category = "Eletro Domestico"
+
+            };
+
+            var p5 = new Product()
+            {
+                Id = "907f191e810c19729de860ea",
+                Name = "Microondas",
+                Description = "Ut sequi officia qui corporis suscipit cum deserunt minus ea quidem expedita et voluptas perferendis sed totam perspiciatis. Et atque officia vel cupiditate rerum ea laboriosam quasi est iste vero est iste cupiditate ea quia doloribus eum unde repellat.",
+                Image = "celulars10e.png",
+                Price = 3500.00M,
+                Category = "Eletro Domestico"
+
+            };
+
+            products.Add(p1);
+            products.Add(p2);
+            products.Add(p3);
+            products.Add(p4);
+            products.Add(p5);
+
+            return products;
+        }
+    }
+}
