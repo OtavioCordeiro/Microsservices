e20d20a86b8b3bf92f55e671317baf849217afcf
Otavio Cordeiro Lopes <otavio.cordeiro.ext@brf.com>
2022-07-24 16:18:37 -0300

    Adiciona serviço discount grpc

--- Discount.Grpc/Program.cs
+++ Discount.Grpc/Program.cs
@@ -1,6 +1,7 @@
 
 using Discount.Grpc.Repositories;
 using Discount.Grpc.Repositories.Interfaces;
+using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
+
+app.MapGrpcService<DiscountService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
