785bfb66c99278745194ed2841f56be5064d929b
Otavio Cordeiro Lopes <otavio.cordeiro.ext@brf.com>
2022-07-24 11:13:07 -0300

    Implementa Dicount.Proto

--- Discount.Grpc/Program.cs
+++ Discount.Grpc/Program.cs
@@ -1,4 +1,6 @@
-using Discount.Grpc.Services;
+
+using Discount.Grpc.Repositories;
+using Discount.Grpc.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +13,9 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ Discount.Grpc/Repositories/DiscountRepository.cs
@@ -0,0 +1,73 @@
+﻿using Dapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Repositories.Interfaces;
+using Npgsql;
+
+namespace Discount.Grpc.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            var connection = GetConnectionPostgreSql();
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                                ("SELECT * FROM Coupon Where productName = @ProductName",
+                                new { ProductName = productName });
+            if (coupon == null)
+                return new Coupon()
+                { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            return coupon;
+        }
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            var connection = GetConnectionPostgreSql();
+
+            var affecetd = await connection.ExecuteAsync("INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount) ",                                                    
+                                                         new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+
+            if (affecetd == 0)
+                return false;
+
+            return true;
+        }
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            var connection = GetConnectionPostgreSql();
+
+            var affecetd = await connection.ExecuteAsync("UPDATE Coupon SET ProductName = @ProductName, Description = @Description, Amount = @Amount " +                                                         
+                                                         "WHERE Id = @Id",
+                                                         new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount, Id = coupon.Id });
+
+            if (affecetd == 0)
+                return false;
+
+            return true;
+        }
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            var connection = GetConnectionPostgreSql();
+
+            var affecetd = await connection.ExecuteAsync("DELETE FROM Coupon WHERE ProductName = @ProductName",
+                                                         new { ProductName = productName });
+
+            if (affecetd == 0)
+                return false;
+
+            return true;
+        }
+
+        private NpgsqlConnection GetConnectionPostgreSql()
+        {
+            return new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+    }
+}
